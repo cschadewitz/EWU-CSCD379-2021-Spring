f1421d2c64f8910f0085ea9bc81212fd146f6a5e
Casey Schadewitz <casey.schadewitz@gmail.com>
2021-04-11 01:29:34 -0700

    Additional Style Changes

--- SecretSanta/src/SecretSanta.Web/tailwind.config.js
+++ SecretSanta/src/SecretSanta.Web/tailwind.config.js
@@ -1,20 +1,15 @@
-﻿module.exports = {
+﻿const colors = require('tailwindcss/colors')
+
+module.exports = {
     purge: ['./**/*.html', './**/*.cshtml'],
     theme: {
         colors: {
-            primary: {
-                0: '#198f17',
-                10: '#169c18',
-                20: '#14a619',
-                30: '#12b11a',
-                40: '#10bb1b',
-                50: '#0ec51c',
-                60: '#0bd11d',
-                70: '#09db1e',
-                80: '#06e71f',
-                90: '#03f220',
-                100: '#00ff21'
-            }
+            transparent: 'transparent',
+            current: 'currentColor',
+            gray: colors.warmGray,
+            red: colors.red,
+            green: colors.green,
+            yellow: colors.yellow,
         },
         extend: {}
     },
