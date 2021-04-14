ee0543b48bd333ed8e738466795bd613885a6b23
Casey Schadewitz <casey.schadewitz@gmail.com>
2021-04-14 01:23:11 -0700

    Add tailwind & postcss config Add Responsive Nav Feature Add Create and Edit Responsive Page Feature Add Page Content Margin(Padding) Add Create and Edit Input Field Error Styling Fix Site Title

---
+++ SecretSanta/src/SecretSanta.Web/tailwind.config.js
@@ -0,0 +1,21 @@
+﻿const colors = require('tailwindcss/colors')
+
+module.exports = {
+    purge: ['./**/*.html', './**/*.cshtml'],
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            gray: colors.warmGray,
+            red: colors.red,
+            green: colors.green,
+            yellow: colors.yellow,
+        },
+        extend: {}
+    },
+    variants: {
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ]
+}
