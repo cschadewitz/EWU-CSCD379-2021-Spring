0deae14c20b5253811c962a99c024347c446eb69
cschadewitz <cschadewitz@eagles.ewu.edu>
2021-05-09 21:34:27 -0700

    Clean up additional warnings

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SecretSanta.Business;
@@ -8,9 +9,11 @@ namespace SecretSanta.Api
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration) { }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddControllers();
@@ -18,7 +21,7 @@ namespace SecretSanta.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
